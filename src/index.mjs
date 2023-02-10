6816fbd44734c3c92f61509169a431bcb328e8f2
gavinzhao <gavinzhao@futunn.com>
2023-02-10 11:40:45 +0800

    feat: support session

--- src/index.mjs
+++ src/index.mjs
@@ -5,19 +5,39 @@ import { koaBody } from 'koa-body';
 const app = new Koa();
 const api = new ChatGPTAPI({
     apiKey: process.env.OPENAI_API_KEY,
-})
+});
+
+const cache = new Map();
 
 app.use(koaBody());
 app.use(async ctx => {
     let question;
+    let sessionId;
     if (ctx.method === 'GET') {
         question = decodeURIComponent(ctx.request.query.text);
+        sessionId = ctx.request.query.sessionId;
     } else {
         question = ctx.request?.body?.text;
+        sessionId = ctx.request?.body?.sessionId;
     }
-    console.log(question, ctx.request.body)
     try {
-        ctx.body = await api.sendMessage(question ?? '介绍自己');
+        const res = await api.sendMessage(question ?? '介绍自己', {
+            conversationId: cache.get(sessionId)?.conversationId,
+            parentMessageId: cache.get(sessionId)?.id,
+        });
+
+        if (sessionId) {
+            if (cache.has(sessionId)) {
+                cache.get(sessionId).id = res.id;
+            } else {
+                cache.set(sessionId, {
+                    conversationId: res.conversationId,
+                    id: res.id,
+                });
+            }
+        }
+
+        ctx.body = res;
     } catch (e) {
         ctx.status = 400;
         ctx.body = {
